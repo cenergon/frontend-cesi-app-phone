bbc5ce210b0de2ce1f4812c7450a8986f4b2dda9
Juan Carlos Ramos <ramossistemas@gmail.com>
2020-01-24 14:19:02 -0300

    Lectura barra ok

--- src/app/pages/tab4/tab4.page.ts
+++ src/app/pages/tab4/tab4.page.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { BarcodeScanner } from '@ionic-native/barcode-scanner/ngx';
+import { BarcodeScanner, BarcodeScannerOptions } from '@ionic-native/barcode-scanner/ngx';
 
 
 @Component({
@@ -13,7 +13,7 @@ export class Tab4Page implements OnInit {
     allowSlidePrev : false,
     allowSlideNext : false
   };
-  
+
   constructor( private barcodeScanner: BarcodeScanner) { }
 
 
@@ -41,7 +41,19 @@ export class Tab4Page implements OnInit {
   }
 
   scan() {
-    this.barcodeScanner.scan().then(barcodeData => {
+
+    const options: BarcodeScannerOptions = {
+      preferFrontCamera: false,
+      showFlipCameraButton: true,
+      showTorchButton: true,
+      torchOn: false,
+      prompt: 'Porfavor coloque el codigo en el recuadro',
+      resultDisplayDuration: 500,
+      formats: 'QR_CODE,PDF_417',
+      orientation: 'landscape',
+    };
+
+    this.barcodeScanner.scan(options).then(barcodeData => {
       console.log('Barcode data', barcodeData);
      }).catch(err => {
          console.log('Error', err);
